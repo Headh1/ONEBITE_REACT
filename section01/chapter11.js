b5fa40210e10fc88650a5766e8c396e7a0b988a5
Headh1 <a.design804@gmail.com>
2026-01-08 22:37:29 +0900

    0108

---
+++ section01/chapter11.js
@@ -0,0 +1,18 @@
+let area1 = getArea(10, 20);
+console.log(area1);
+
+let area2 = getArea(30, 20);
+console.log(area2);
+
+getArea(120, 200);
+
+function getArea(width, height) {
+  function another() {
+    console.log("another");
+  }
+
+  another();
+  let area = width * height;
+
+  return area;
+}
